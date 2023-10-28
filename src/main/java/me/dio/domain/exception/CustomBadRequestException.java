660b7adde814dc3c402ba3437df8c2bc1048f2be
marcos <marcsalexandr@gmail.com>
2023-10-28 02:49:43 -0300

    Revert "bootcamp_estacionamento"
    This reverts commit 8fcd9d7036cc2e94f6eb27e49f84e2531882011a.

--- src/main/java/me/dio/domain/exception/CustomBadRequestException.java
+++
@@ -1,7 +0,0 @@
-package me.dio.domain.exception;
-
-public class CustomBadRequestException extends RuntimeException {
-    public CustomBadRequestException(String message) {
-        super(message);
-    }
-}
